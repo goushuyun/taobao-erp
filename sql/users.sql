39f05832ec20a41320ad49189c3df53002ed0277
Wang-Kai <persistence2013@sina.com>
2017-07-30 14:34:58 +0800

    complete User Register function

--- sql/users.sql
+++ sql/users.sql
@@ -1,8 +1,13 @@
 create SEQUENCE user_id_seq;
 
 create table users (
-    "id" text primary key not null default to_char(now() AT TIME ZONE 'cct', 'yymmdd') || trim(to_char(nextval('user_id_seq'), '00000')),
-    tel text not null,
+    "id" text primary key not null default  'u_' || to_char(now() AT TIME ZONE 'cct', 'yymmdd') || trim(to_char(nextval('user_id_seq'), '00000')),
+
+    mobile text not null,
     password text not null,
-    role int not null
+    name text not null,
+    role int not null default 256,
+
+    create_at timestamptz not null default now(),
+    update_at timestamptz not null default now()
 );
--- users/service/users.go
+++ users/service/users.go
@@ -1,13 +1,55 @@
 package service
 
 import (
+	"fmt"
+
+	"github.com/goushuyun/taobao-erp/db"
+	"github.com/goushuyun/taobao-erp/errs"
 	"github.com/goushuyun/taobao-erp/pb"
+	users_db "github.com/goushuyun/taobao-erp/users/db"
 	"golang.org/x/net/context"
+
+	"github.com/goushuyun/taobao-erp/misc/token"
+	"github.com/goushuyun/taobao-erp/users/role"
+	"github.com/wothing/log"
 )
 
 type UsersServer struct {
 }
 
 func (s *UsersServer) Register(ctx context.Context, req *pb.User) (*pb.UserResp, error) {
-	return nil, nil
+	// check if identifying code is ok
+	conn := db.GetRedisConn()
+	defer conn.Close()
+
+	checkcode, err := conn.Do("get", req.Mobile)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	// checkcode is expired
+	if checkcode == nil {
+		return &pb.UserResp{Code: errs.Ok, Message: "checkcode_expired"}, nil
+	}
+
+	// checkcode is error
+	checkcode = fmt.Sprintf("%s", checkcode)
+	if checkcode != req.Checkcode {
+		return &pb.UserResp{Code: errs.Ok, Message: "checkcode_error"}, nil
+	}
+
+	// insert user to db
+	err = users_db.SaveUser(req)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	// sign token
+	token := token.SignUserToken(role.InterNormalUser, req.Id)
+
+	log.Debug(token)
+
+	return &pb.UserResp{Code: errs.Ok, Message: "ok", Data: req, Token: token}, nil
 }
