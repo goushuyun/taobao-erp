12c249605954ced1a0a0c56039296e7cbef2e875
oricanIs <492226568@qq.com>
2017-07-31 21:58:38 +0800

    add new interface for update book info  "

--- gateway/controller/book.go
+++ gateway/controller/book.go
@@ -18,3 +18,9 @@ func SaveBook(w http.ResponseWriter, r *http.Request) {
 	req := &pb.Book{}
 	misc.CallWithResp(w, r, "book", "SaveBook", req, "isbn", "price", "title")
 }
+
+//get book info
+func UpdateBookInfo(w http.ResponseWriter, r *http.Request) {
+	req := &pb.Book{}
+	misc.CallWithResp(w, r, "book", "UpdateBookInfo", req, "id")
+}
