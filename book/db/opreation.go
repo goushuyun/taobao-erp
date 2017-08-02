bbf427e18e32b10b4ad5321ee0b293dfd175b4e4
oricanIs <492226568@qq.com>
2017-08-02 07:40:35 +0000

    add

--- book/db/opreation.go
+++ book/db/opreation.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/wothing/log"
@@ -29,7 +30,7 @@ func SaveBookAudit(record *pb.BookAuditRecord) error {
 /*
    get book audit list for Auditor
 */
-func GetBookAuditList(record *pb.BookAuditRecord) {
+func GetBookAuditList(record *pb.BookAuditRecord) (records []*pb.BookAuditRecord, err error, totalCount int64) {
 	query := "select id,book_id,isbn,book_cate,title,publisher,author,edition,image,price,apply_user_id,apply_user_name,check_user_id,check_user_name,apply_reason,status,feedback,extract(epoch from create_at)::bigint from book_audit_record where 1=1"
 	queryCount := "select count(*) from book_audit_record where 1=1"
 	var condition string
@@ -51,22 +52,185 @@ func GetBookAuditList(record *pb.BookAuditRecord) {
 	}
 	// condition 5 : isbn
 	if record.Isbn != "" {
-
+		condition += fmt.Sprintf(" and isbn='%s'", record.Isbn)
 	}
+
 	// condition 6 : status
 	if record.Status != 0 {
 		condition += fmt.Sprintf(" and status=%d", record.Status)
 	}
+	//condition 7 : book_cate
+	if record.BookCate != "" {
+		condition += fmt.Sprintf(" and book_cate='%s'", record.BookCate)
+	}
 	queryCount += condition
 	log.Debug(queryCount)
+
+	// find out how rows tyey are
+	err = DB.QueryRow(query).Scan(&totalCount)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	if totalCount <= 0 {
+		return
+	}
+	if record.Page <= 0 {
+		record.Page = 1
+	}
+	if record.Size <= 0 {
+		record.Size = 15
+	}
+	// ergodic the rows
+	condition += fmt.Sprintf(" order by create_at,id offset %d limit %d", (record.Page-1)*record.Size, record.Size)
 	query += condition
-	log.Debug(query)
+	rows, err := DB.Query(query)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		model := &pb.BookAuditRecord{}
+		records = append(records, model)
+		//id,book_id,isbn,book_cate,title,publisher,author,edition,image,price,apply_user_id,apply_user_name,check_user_id,check_user_name,apply_reason,status,feedback,extract(epoch from create_at)::bigint
+		err = rows.Scan(&model.Id, &model.BookId, &model.Isbn, &model.BookCate, &model.Title, &model.Publisher, &model.Author, &model.Edition, &model.Image, &model.Price, &model.ApplyUserId, &model.ApplyUserName, &model.CheckUserId, &model.CheckUserName, &model.ApplyReason, &model.Status, &model.Feedback, &model.CreateAt)
+		if err != nil {
+			log.Error(err)
+			return
+		}
+	}
+
+	return
 
 }
 
 /*
    update book audit
 */
-func UpdateBookAudit() {
+func UpdateBookAudit(record *pb.BookAuditRecord) error {
+	query := "update book_audit_record set update_at=now()"
+	var condition string
+	if record.CheckUserId != "" {
+		condition += fmt.Sprintf(",check_user_id='%s'", record.CheckUserId)
+	}
+	if record.CheckUserName != "" {
+		condition += fmt.Sprintf(",check_user_name='%s'", record.CheckUserName)
+	}
+	if record.Status != 0 {
+		condition += fmt.Sprintf(",status=%d", record.Status)
+	}
+	if record.Feedback != "" {
+		condition += fmt.Sprintf(",feedback='%s'", record.Feedback)
+	}
+	condition += fmt.Sprintf(" where id='%s'", record)
+	query += condition
+	log.Debug(query)
+	result, err := DB.Exec(query)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+	rowsCount, err := result.RowsAffected()
+	if err != nil {
+		log.Error(err)
+	}
+	if rowsCount <= 0 {
+		return errors.New("没修改任何数据呦～")
+	}
+	return nil
+}
 
+// get book audit organize list
+func GetOrganizedBookAuditList(record *pb.BookAuditRecord) (models []*pb.OrganizedBookAudit, err error, totalCount int64) {
+	var query, queryCount string
+	if record.SearchType == 0 {
+		// update
+		query = fmt.Sprintf("select book_id,count(*) from book_audit_record where status=1 group by book_id order by create_at,id offset %d limit %d", (record.Page-1)*record.Size, record.Size)
+		queryCount = "select count(distinct book_id) from book_audit_record where status=1"
+		log.Debug(queryCount)
+		err = DB.QueryRow(queryCount).Scan(&totalCount)
+		if err != nil {
+			log.Error(err)
+			return
+		}
+		if totalCount <= 0 {
+			return
+		}
+		log.Debug(query)
+		rows, errNw := DB.Query(query)
+		if errNw != nil {
+			log.Error(errNw)
+			err = errNw
+			return
+		}
+		defer rows.Close()
+		for rows.Next() {
+			model := &pb.OrganizedBookAudit{}
+			models = append(models, model)
+			err = rows.Scan(&model.BookId, &model.ParticipateNum)
+			if err != nil {
+				log.Error(err)
+				return
+			}
+		}
+
+	} else {
+		// insert
+		query = fmt.Sprintf("select isbn,count(*) from book_audit_record where status=1 and book_cate<>'' group by book_id order by create_at,id offset %d limit %d", (record.Page-1)*record.Size, record.Size)
+		queryCount = "select count(distinct isbn) from book_audit_record where status=1 and book_cate<>''"
+		log.Debug(queryCount)
+		err = DB.QueryRow(queryCount).Scan(&totalCount)
+		if err != nil {
+			log.Error(err)
+			return
+		}
+		if totalCount <= 0 {
+			return
+		}
+
+		log.Debug(query)
+		rows, errNw := DB.Query(query)
+		if errNw != nil {
+			log.Error(errNw)
+			err = errNw
+			return
+		}
+		defer rows.Close()
+		for rows.Next() {
+			model := &pb.OrganizedBookAudit{}
+			models = append(models, model)
+			err = rows.Scan(&model.Isbn, &model.ParticipateNum)
+			if err != nil {
+				log.Error(err)
+				return
+			}
+		}
+	}
+
+	//organize the result and return
+	for i := 0; i < len(models); i++ {
+		model := models[i]
+		book := &pb.Book{Isbn: model.Isbn, Id: model.BookId}
+		bookList, err := GetBookInfo(book)
+		if err != nil {
+			log.Error(err)
+			return models, err, totalCount
+		}
+		if len(bookList) > 0 {
+			book = bookList[1]
+			model.BookId = book.Id
+			model.Isbn = book.Isbn
+			model.BookCate = book.BookCate
+			model.BookNo = book.BookNo
+			model.Image = book.Image
+			model.Title = book.Title
+			model.Price = book.Price
+			model.Publisher = book.Publisher
+			model.Author = book.Author
+			model.Edition = book.Edition
+		}
+	}
+
+	return
 }
--- book/service/operation.go
+++ book/service/operation.go
@@ -1,12 +1,14 @@
 package service
 
 import (
+	"errors"
+
 	"golang.org/x/net/context"
 
+	"github.com/goushuyun/taobao-erp/book/db"
 	"github.com/goushuyun/taobao-erp/errs"
 	"github.com/goushuyun/taobao-erp/misc"
 	"github.com/goushuyun/taobao-erp/pb"
-
 	"github.com/wothing/log"
 )
 
@@ -14,7 +16,21 @@ import (
 func (s *BookServer) SubmitBookAudit(ctx context.Context, in *pb.BookAuditRecord) (*pb.NormalResp, error) {
 	tid := misc.GetTidFromContext(ctx)
 	defer log.TraceOut(log.TraceIn(tid, "SubmitBookAudit", "%#v", in))
-
+	if in.BookId != "" {
+		_, err, totalCount := db.GetBookAuditList(in)
+		if err != nil {
+			log.Error(err)
+			return nil, errs.Wrap(errors.New(err.Error()))
+		}
+		if totalCount > 0 {
+			return nil, errs.Wrap(errors.New("你有该书未审核的图书信息，请勿重复提交"))
+		}
+	}
+	err := db.SaveBookAudit(in)
+	if err != nil {
+		log.Error(err)
+		return nil, errs.Wrap(errors.New(err.Error()))
+	}
 	return &pb.NormalResp{Code: errs.Ok, Message: "ok"}, nil
 }
 
@@ -22,14 +38,34 @@ func (s *BookServer) SubmitBookAudit(ctx context.Context, in *pb.BookAuditRecord
 func (s *BookServer) GetBookAuditRecord(ctx context.Context, in *pb.BookAuditRecord) (*pb.BookAuditRecordListResp, error) {
 	tid := misc.GetTidFromContext(ctx)
 	defer log.TraceOut(log.TraceIn(tid, "GetBookAuditRecord", "%#v", in))
-
-	return &pb.BookAuditRecordListResp{Code: errs.Ok, Message: "ok"}, nil
+	models, err, totalCount := db.GetBookAuditList(in)
+	if err != nil {
+		log.Error(err)
+		return nil, errs.Wrap(errors.New(err.Error()))
+	}
+	return &pb.BookAuditRecordListResp{Code: errs.Ok, Message: "ok", Data: models, TotalCount: totalCount}, nil
 }
 
 // handle the audit request : accept or reject (when reject ,a reason better for user)
 func (s *BookServer) UpdateAuditRecord(ctx context.Context, in *pb.BookAuditRecord) (*pb.NormalResp, error) {
 	tid := misc.GetTidFromContext(ctx)
 	defer log.TraceOut(log.TraceIn(tid, "UpdateAuditRecord", "%#v", in))
-
+	err := db.UpdateBookAudit(in)
+	if err != nil {
+		log.Error(err)
+		return nil, errs.Wrap(errors.New(err.Error()))
+	}
 	return &pb.NormalResp{Code: errs.Ok, Message: "ok"}, nil
+}
+
+// get book audit organize list
+func (s *BookServer) GetOrganizedBookAuditList(ctx context.Context, in *pb.BookAuditRecord) (*pb.OrganizedBookAuditListResp, error) {
+	tid := misc.GetTidFromContext(ctx)
+	defer log.TraceOut(log.TraceIn(tid, "GetOrganizedBookAuditList", "%#v", in))
+	models, err, totalCount := db.GetOrganizedBookAuditList(in)
+	if err != nil {
+		log.Error(err)
+		return nil, errs.Wrap(errors.New(err.Error()))
+	}
+	return &pb.OrganizedBookAuditListResp{Code: errs.Ok, Message: "ok", Data: models, TotalCount: totalCount}, nil
 }
